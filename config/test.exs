dba936060a2760ec809004ef8ec3a6ce4ff0b46e
Scott Macfarlane <scott@gina.alaska.edu>
2016-06-22 15:20:21 -0800

    Work through Chapter 8

--- config/test.exs
+++ config/test.exs
@@ -12,8 +12,9 @@ config :logger, level: :warn
 # Configure your database
 config :rumbl, Rumbl.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
   database: "rumbl_test",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
+
+config :comeonin, :bcrypt_log_rounds, 4
+config :comeonin, :pbkdf2_rounds, 1
